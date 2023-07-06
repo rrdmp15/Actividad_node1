17906484b6cc8dba89acc34d3fe3a68e55be488f
rrdmp15 <rdayismar@gmail.com>
2023-07-06 08:42:01 -0500

    feat: :sparkles: Post bodegas
    se hace el endpoint para agregar una nueva bodega

--- routers/bodegas.js
+++ routers/bodegas.js
@@ -18,7 +18,23 @@ storageBodegas.get('/', (req, res)=>{
     )
 })
 
-
+storageBodegas.post('/', (req, res) => {
+    const { nombre, id_responsable, estado, created_by, update_by, updated_at, deleted_at } = req.body;
+    
+    con.query(
+      /*sql*/ `INSERT INTO bodegas (nombre, id_responsable, estado, created_by, update_by, updated_at, deleted_at) 
+      VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [nombre, id_responsable, estado, created_by, update_by, updated_at, deleted_at],
+      (err, data, fil) => {
+        if (err) {
+          console.error(err);
+          res.sendStatus(500);
+        } else {
+          res.send(JSON.stringify(data));
+        }
+      }
+    );
+  });
   
 
 export default storageBodegas;
